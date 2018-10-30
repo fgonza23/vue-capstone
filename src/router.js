08dc7b3b0587006e92a9865eb23e27149ded30ba
fgonza23 <fgonza0267@gmail.com>
2018-10-30 18:00:36 -0500

    created vue capstone, will have to add axios call

--- src/router.js
+++ src/router.js
@@ -1,6 +1,12 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Signup from "./views/Signup.vue";
+import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
+
+
+
 
 Vue.use(Router)
 
@@ -18,6 +24,22 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    { path: "/signup",
+     name: "signup",
+      component: Signup
+    },
+    { 
+    path: "/login", 
+    name: "login", 
+    component: Login 
+    },
+    { 
+      path: "/logout", 
+      name: "logout", 
+      component: Logout 
     }
+
+
   ]
 })
